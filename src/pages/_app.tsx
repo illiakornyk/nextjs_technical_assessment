2833c18a59f6f5da1bfd631107a3517ec60225a9
Illia Kornyk <krekerbrow@gmail.com>
2023-08-10 12:07:17 +0300

    feat: install fonts and configure globals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import Layout from "@/components/layout";
-import type { AppProps } from "next/app";
+import Layout from '@/components/layout';
+import type { AppProps } from 'next/app';
+import '@/styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
