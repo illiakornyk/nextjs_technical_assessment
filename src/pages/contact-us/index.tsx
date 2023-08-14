5984208db25c509ad71ac3b114f01b5ab9436dea
Illia Kornyk <krekerbrow@gmail.com>
2023-08-14 13:37:40 +0300

    feat: create icons for social medias

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -5,5 +5,37 @@ import styles from './styles.module.scss';
 let cx = classNames.bind(styles);
 
 export default function ContactUsPage() {
-  return <div className={cx('contact-us-page')}></div>;
+  return (
+    <div className={cx('contact-us-page')}>
+      <h2>Contact Us</h2>
+      <p>Any question or remarks? Just write us a message!</p>
+
+      <div>
+        <div>
+          <h3>Contact Information</h3>
+          <span>Say something to start a live chat!</span>
+
+          <ul>
+            <li>+1012 3456 789</li>
+            <li>demo@gmail.com</li>
+            <li>
+              132 Dartmouth Street Boston, Massachusetts 02156 United States
+            </li>
+          </ul>
+
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div>
+          <form
+            action="
+			"
+          ></form>
+        </div>
+      </div>
+    </div>
+  );
 }
